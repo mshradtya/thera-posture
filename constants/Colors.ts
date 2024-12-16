37ce4a7f9685d62832caed3bd2978b84506dca4d
Aditya Mishra <mshradtya@gmail.com>
2024-12-16 17:38:52 +0530

    get started page implemented

---
+++ constants/Colors.ts
@@ -0,0 +1,11 @@
+export const Colors = {
+  background: "#F4F4F4",
+  tint: "#4d68fb",
+  tabIconDefault: "#687076",
+  tabIconSelected: "#FF4C4C",
+  white: "#fff",
+  black: "#333",
+  darkGrey: "#666",
+  lightGrey: "#999",
+  softText: "#555",
+};
